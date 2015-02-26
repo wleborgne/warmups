3f20b79ce147787a8f9a8dd30cfeb8117f4ebfff
Bill LeBorgne <bill@leborgne.name>
2015-02-26 13:05:44 -0700

    [#89160332] Add deck_of_cards.rb
    Includes commented out suggestions for changes based
    on Jasons in-class example.

--- 2015-02-25/deck_of_cards.rb
+++ 2015-02-25/deck_of_cards.rb
@@ -3,6 +3,13 @@
 # Implement a playing card
 class Card
   attr_reader :suit, :face_value
+  
+  # CLUB = "\u2663"
+  # DIAMOND = "\u2666"
+  # SPADE = "\u2660"
+  # HEART = "\2764"
+  # then create via Card.new(2, Card::CLUB)
+  # How to use this in Deck?
   def initialize(face_value, suit)
     @face_value = face_value
     @suit = suit
@@ -11,11 +18,21 @@ class Card
   def to_s
     "#{face_value} of #{suit}"
   end
+  
+  # def <=>(other)
+  #   if suit == other.suit
+  #     Card.values.index(value) <=> Card.values.index(other.value)
+  #   else
+  #    Cards.suits.index(suit) <=> Card.suits.index(other.suit)
+  #   end
+  # end
 end
 
 # Implement a standard deck of cards
 class Deck
+  # These should be in Card?
   SUITS = %w(Spades Hearts Clubs Diamonds)
+  # SUITS = %w(Card::CLUB Card::DIAMOND Card::SPADE Card::HEART)
   VALUES = %w(Ace 2 3 4 5 6 7 8 9 10 Jack Queen King)
 
   def initialize
@@ -29,15 +46,31 @@ class Deck
   end
 
   def deal
-    @deck.pop
+    if @deck.length > 0
+      @deck.pop
+    else
+      'No more cards'  
+    end
+  end
+
+  def random_card
+    @deck.delete_at(rand(52))
   end
 
   def to_s
-    @deck.each(&:to_s)
+    if @deck.length > 0
+      @deck.each(&:to_s)
+    else
+      "Empty deck"
+    end
   end
 
   def count
     @deck.count
+  end
+  
+  def sort!
+    @deck.sort!
   end
 end
 
@@ -51,4 +84,6 @@ if __FILE__ == $PROGRAM_NAME
   puts "Here's a card: #{my_deck.deal}"
   puts "Here's another: #{my_deck.deal}"
   puts "There are now #{my_deck.count} cards left."
+  puts "Random card: #{my_deck.random_card}"
+  puts "#{my_deck.count} cards left."
 end
